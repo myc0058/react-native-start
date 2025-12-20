52dc14ed972624bde75b71d430bcde2e60cc750f
Brown <myc0058@gmail.com>
2025-12-20 15:24:19 +0900

    eas setting, defaultFormatter

--- app.config.js
+++ app.config.js
@@ -2,9 +2,16 @@ module.exports = {
   expo: {
     extra: {
       GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-      "eas": {
-        "projectId": "98fafd01-0bbc-401f-b9dc-748558b98018"
-      }
+      eas: {
+        projectId: "98fafd01-0bbc-401f-b9dc-748558b98018",
+      },
+      android: {
+        package: "com.yourcompany.yourappname", // Add this line
+        // ... other android properties (e.g., googleServicesFile, permissions)
+      },
+      ios: {
+        bundleIdentifier: "com.yourcompany.yourappname", // It is also good practice to define the iOS bundle identifier
+      },
     },
   },
 };
